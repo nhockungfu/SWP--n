de5df42f9d749967f481c2a12bbb51a76a490b73
nhockungfu <vsp1308@gmail.com>
2018-05-24 23:13:49 +0700

    update

--- Source_Code/app.js
+++ Source_Code/app.js
@@ -92,9 +92,14 @@ app.use('/quanlisanphamtaikhoan',quanlisanphamtaikhoan);
 
 app.use(handle404);
 
-app.listen(process.env.PORT || 3000,function () {
+var server = app.listen(process.env.PORT || 6000,function () {
     console.log('Sever Is Running');
+	server.close(function () {
+		console.log('[x] Server Stopped');
+	});
    // a.KiemTraSanPham();
 });
 
+
+//server.close();
 module.exports = app;
